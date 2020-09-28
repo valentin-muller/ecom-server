ae57ada34c211b2435a95e44d3aaf3aa9bc7ab91
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-09-28 17:35:17 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+require('dotenv').config({
+    path: './config/index.env'
+})
+
+// Middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('text route => home page');
+});
+
+// Page Not Found
+app.use((req, res) => {
+    res.status(404).json({
+        msg: "Page not found"
+    })
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
+
